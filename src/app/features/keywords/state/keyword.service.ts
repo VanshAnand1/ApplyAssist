c59f8556b1e83ebf4c1e047c0c6bd4a7d1b253fb
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-10-07 15:58:08 -0400

    AA-2 fix: altered KeywordService

--- src/app/features/keywords/state/keyword.service.ts
+++ src/app/features/keywords/state/keyword.service.ts
@@ -1,27 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, computed } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class KeywordService {
-  private keywords: string[] = [];
-
-  private constructor() {
-    this.keywords = [];
-  }
-
-  getInstance() {
-    if (this.keywords == null) {
-      KeywordService.constructor();
-    }
-    return this.keywords;
-  }
+  readonly keywords = signal<string[]>([]);
+  readonly count = computed(() => this.keywords().length);
 
   addKeyword(keyword: string) {
-    this.keywords.push(keyword);
+    this.keywords.update((prev) => [...prev, keyword]);
   }
 
   getIndex(keyword: string) {
-    for (let i = 0; i < this.keywords.length; i++) {
-      if (this.keywords[i] === keyword) {
+    for (let i = 0; i < this.keywords().length; i++) {
+      if (this.keywords()[i] === keyword) {
         return i;
       }
     }
@@ -37,15 +27,17 @@ export class KeywordService {
     } else {
       indexToRemove = keyword;
     }
-    this.keywords.splice(indexToRemove, 1);
+    this.keywords.update((prev) => prev.filter((_, i) => i !== indexToRemove));
   }
 
   removeAllInstances(keyword: string) {
-    this.keywords = this.keywords.filter((str: string) => str !== keyword);
+    this.keywords.update((prev) =>
+      prev.filter((str: string) => str !== keyword)
+    );
   }
 
   clearKeywords() {
-    this.keywords = [];
+    this.keywords.update(() => []);
   }
 
   getKeywords() {
