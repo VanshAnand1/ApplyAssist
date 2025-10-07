0260ae5fd297cbb5290c7707834452ac17911f98
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-10-07 13:49:39 -0400

    AA-1 feature: connected html css ts files together with form

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 import HeaderComponent from './header/header.component';
+import PercentageComponent from './percentage/percentage.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
-  template: ` <header-component></header-component> `,
+  imports: [HeaderComponent, PercentageComponent],
+  template: `
+    <header-component></header-component>
+    <percentage-component></percentage-component>
+  `,
 })
 export default class HomeComponent {}
