e9ae6f27abda9deee3c2a2af5266b39db444cf51
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-10-15 13:16:51 -0400

    AA-4 chore: removed old css styling

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-import HeaderComponent from './header/header.component';
 import PercentageComponent from './keyword-form/percentage/percentage.component';
 import KeywordInputComponent from './keyword-form/keyword-input/keyword-input.component';
 import KeywordDisplayComponent from './keyword-form/keyword-display/keyword-display.component';
@@ -9,7 +8,6 @@ import WindowsComponent from './windows/windows.component';
 @Component({
   selector: 'app-home',
   imports: [
-    HeaderComponent,
     PercentageComponent,
     KeywordInputComponent,
     KeywordDisplayComponent,
@@ -17,7 +15,6 @@ import WindowsComponent from './windows/windows.component';
   ],
   template: `
     <windows-component></windows-component>
-    <header-component></header-component>
     <percentage-component></percentage-component>
     <keyword-input></keyword-input>
     <keyword-display></keyword-display>
