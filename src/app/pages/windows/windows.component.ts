dd1b46df24857625c952a7dae8bfefb3ad53d713
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-10-17 00:12:50 -0400

    AA-4 feature: new window name field

--- src/app/pages/windows/windows.component.ts
+++ src/app/pages/windows/windows.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { inject } from '@angular/core';
 import { WindowService } from '../../features/windows/state/window.service';
-import { StorageService } from 'src/app/features/storage/storage.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'windows-component',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './windows.component.html',
   styleUrls: ['./windows.component.css'],
 })
 export default class WindowsComponent {
+  windowName: string = '';
   windowService = inject(WindowService);
 
+  onInputChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.windowName = inputElement.value;
+  }
+
   createNewWindow() {
-    this.windowService.createNewWindow('blue', '');
+    this.windowService.createNewWindow('blue', this.windowName);
   }
 
   onWindowClick(windowID: string) {
