3459ee9098e7c350cd64a7bd1607f18fd4314dff
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-10-18 19:45:51 -0400

    AA-5 feature: added scripting permissions and connected to DOM

--- src/app/pages/windows/windows.component.ts
+++ src/app/pages/windows/windows.component.ts
@@ -3,6 +3,7 @@ import { inject } from '@angular/core';
 import { WindowService } from '../../features/windows/state/window.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import KeywordFormComponent from '../keyword-form/keyword-form.component';
 
 @Component({
   selector: 'windows-component',
@@ -33,7 +34,28 @@ export default class WindowsComponent {
     this.windowService.removeWindow(windowID);
   }
 
-  pinWindow(windowID: string) {
-    console.log('pin', windowID);
+  async getActiveTabID() {
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+    return tab?.id;
+  }
+
+  async pinWindow(windowID: string) {
+    const tabId = await this.getActiveTabID();
+    if (!tabId) return;
+
+    chrome.scripting.executeScript({
+      target: { tabId },
+      func: (id) => {
+        console.log('hello');
+        // const overlayWindowID = 'APPLYASSIST-OVERLAY-WINDOW-ID';
+        // let overlayWindow = document.getElementById(overlayWindowID);
+        // overlayWindow = document.createElement('keyword-form-component');
+        // document.documentElement.appendChild(overlayWindow);
+      },
+      args: [windowID],
+    });
   }
 }
