76f8eb20132cebb851384eb7005cc4dad32d1ef7
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-11-02 11:04:48 -0500

    AA-6 feature: initialized beforeEach and afterEach

--- src/app/features/keywords/state/keyword.service.spec.ts
+++ src/app/features/keywords/state/keyword.service.spec.ts
@@ -1,9 +1,42 @@
 import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
 import { KeywordService } from './keyword.service';
 import { StorageServiceMock, WindowServiceMock } from 'src/testing/types';
+import {
+  createStorageServiceMock,
+  createWindowServiceMock,
+} from 'src/testing/helpers';
+import { TestBed } from '@angular/core/testing';
+import { StorageService } from '../../storage/storage.service';
+import { WindowService } from '../../windows/state/window.service';
 
 describe('KeywordService', () => {
   let service: KeywordService;
   let storage: StorageServiceMock;
   let windowService: WindowServiceMock;
+
+  beforeEach(() => {
+    storage = createStorageServiceMock();
+    windowService = createWindowServiceMock();
+
+    TestBed.configureTestingModule({
+      providers: [
+        KeywordService,
+        {
+          provide: StorageService,
+          useValue: storage,
+        },
+        {
+          provide: WindowService,
+          useValue: windowService,
+        },
+      ],
+    });
+
+    service = TestBed.inject(KeywordService);
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+    TestBed.resetTestingModule();
+  });
 });
